1ff631990186d8be6fe7da76886878aef3864cde
Jannis Pohlmann <jannis@edgeandnode.com>
2021-07-07 15:26:00 +0200

    gateway: Fix a few things

--- gateway/gateway-agent.sh
+++ gateway/gateway-agent.sh
@@ -1,34 +1,33 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 ########################################################################
 # Configuration
 
-GATEWAY_SOURCES=/path/to/local/gateway/repo
-GATEWAY_LOG_LEVEL=trace
+export GATEWAY_SOURCES=/path/to/gateway
+export GATEWAY_LOG_LEVEL=trace
 
 # Local Postgres
-GATEWAY_POSTGRES_USERNAME=
+export GATEWAY_POSTGRES_USERNAME=
 
 # Get these from Jannis or Martynas
-GATEWAY_MNEMONIC=
-GATEWAY_STUDIO_DATABASE_PASSWORD=
-GATEWAY_NETWORK_SUBGRAPH_AUTH_TOKEN=
+export GATEWAY_MNEMONIC=
+export GATEWAY_STUDIO_DATABASE_PASSWORD=
+export GATEWAY_NETWORK_SUBGRAPH_AUTH_TOKEN=
 
 # Mainnet
-GATEWAY_ETHEREUM=https://eth-mainnet.alchemyapi.io/v2/mWSH9YlhpXfXymzLxptC1TE2CIy2QuMA \
-GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.network.thegraph.com/network
+export GATEWAY_ETHEREUM=https://eth-mainnet.alchemyapi.io/v2/mWSH9YlhpXfXymzLxptC1TE2CIy2QuMA \
+export GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.network.thegraph.com/network
 
 # Testnet
-# GATEWAY_ETHEREUM=https://eth-rinkeby.alchemyapi.io/v2/MbPzjkEmyF891zBE51Q1c5M4IQAEXZ-9 \
-# GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.testnet.thegraph.com/network
+# export GATEWAY_ETHEREUM=https://eth-rinkeby.alchemyapi.io/v2/MbPzjkEmyF891zBE51Q1c5M4IQAEXZ-9 \
+# export GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.testnet.thegraph.com/network
 
 ########################################################################
 # Run
 
-NODE_ENV=development
+export NODE_ENV=development
 
 # Ensure a local gateway database exists
 (createdb gateway 2>/dev/null || true)
@@ -45,7 +44,7 @@ yarn agent \
   --name gateway-local \
   --metrics-port 7302 \
   --gateway http://localhost:6700/ \
-  --gateway-postgres gateway \
+  --postgres-database gateway \
   --ethereum-networks mainnet:15:https://eth-mainnet.alchemyapi.io/v2/mWSH9YlhpXfXymzLxptC1TE2CIy2QuMA \
   --sync-allocations-interval 10000 \
   --minimum-indexer-version 0.15.0 \
--- gateway/gateway.sh
+++ gateway/gateway.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 ########################################################################
 # Configuration
 
-GATEWAY_SOURCES=/path/to/local/gateway/repo
+export GATEWAY_SOURCES=/path/to/gateway
+export GATEWAY_LOG_LEVEL=debug
 
 # Get these from Jannis or Martynas
-GATEWAY_MNEMONIC=
-GATEWAY_STATS_DATABASE_HOST=
-GATEWAY_STATS_DATABASE_PASSWORD=
-GATEWAY_NETWORK_SUBGRAPH_AUTH_TOKEN=
+export GATEWAY_MNEMONIC=
+export GATEWAY_STATS_DATABASE_HOST=
+export GATEWAY_STATS_DATABASE_PASSWORD=
+export GATEWAY_NETWORK_SUBGRAPH_AUTH_TOKEN=
 
 # Mainnet
-GATEWAY_ETHEREUM=https://eth-mainnet.alchemyapi.io/v2/mWSH9YlhpXfXymzLxptC1TE2CIy2QuMA \
-GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.network.thegraph.com/network
+export GATEWAY_ETHEREUM=https://eth-mainnet.alchemyapi.io/v2/mWSH9YlhpXfXymzLxptC1TE2CIy2QuMA \
+export GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.network.thegraph.com/network
 
 # Testnet
-# GATEWAY_ETHEREUM=https://eth-rinkeby.alchemyapi.io/v2/MbPzjkEmyF891zBE51Q1c5M4IQAEXZ-9 \
-# GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.testnet.thegraph.com/network
+# export GATEWAY_ETHEREUM=https://eth-rinkeby.alchemyapi.io/v2/MbPzjkEmyF891zBE51Q1c5M4IQAEXZ-9 \
+# export GATEWAY_NETWORK_SUBGRAPH_ENDPOINT=https://network-subgraph-indexers.testnet.thegraph.com/network
 
 ########################################################################
 # Run
 
-NODE_ENV=development
+export NODE_ENV=development
 
 cd $GATEWAY_SOURCES
 
