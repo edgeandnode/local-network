0ac5c7bcb7595b06d27e16fe8e523ef837b65520
Theo Butler <theodusbutler@gmail.com>
2021-12-08 09:44:32 -0500

    Reintroduce patch for common-ts

--- patch.bash
+++ patch.bash
@@ -40,6 +40,7 @@ find_replace_sed '_src\/\*.ts' '_src\/types\/\*.ts' scripts/prepublish
 popd
 
 pushd projects/graphprotocol/common-ts/packages/common-ts/
+git switch ford/local-network
 find_replace_yalc @ethersproject/contracts @graphprotocol/contracts package.json
 popd
 
