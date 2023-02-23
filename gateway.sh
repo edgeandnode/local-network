cf2d3d1e3e5774ec15f13cbe7232d2facc290646
Theo Butler <theodusbutler@gmail.com>
2023-02-23 18:32:34 -0500

    Add gateway TOML

--- gateway.sh
+++ gateway.sh
@@ -11,28 +11,9 @@ await_ready graph-subgraph
 await "test -f build/studio-admin-auth.txt"
 
 export STUDIO_AUTH=$(cat build/studio-admin-auth.txt)
+envsubst <gateway.toml >build/gateway.toml
 
 cd build/edgeandnode/graph-gateway
 
 export RUST_LOG=info,graph_gateway=trace
-export LOG_JSON=false
-export MNEMONIC="${MNEMONIC}"
-export ETHEREUM_PROVIDERS="${ETHEREUM_NETWORK}=3,http://localhost:${ETHEREUM_PORT}"
-export NETWORK_SUBGRAPH="http://localhost:${GRAPH_NODE_GRAPHQL_PORT}/subgraphs/id/${NETWORK_SUBGRAPH_DEPLOYMENT}"
-export GRAPH_ENV_ID=localnet
-export IPFS="http://localhost:${IPFS_PORT}/api/v0/cat?arg="
-# export RESTRICTED_DEPLOYMENTS="${NETWORK_SUBGRAPH_DEPLOYMENT}=${ACCOUNT_ADDRESS}"
-
-export STUDIO_URL="http://localhost:${STUDIO_ADMIN_PORT}/admin/v1"
-export API_KEY_PAYMENT_REQUIRED=true
-
-export PORT="${GATEWAY_PORT}"
-export METRICS_PORT="${GATEWAY_METRICS_PORT}"
-
-export REDPANDA_BROKERS="localhost:${REDPANDA_PORT}"
-export FISHERMAN="http://localhost:${FISHERMAN_PORT}"
-
-export LOCATION_COUNT=1
-export REPLICA_COUNT=1
-
-cargo run --bin graph-gateway
+cargo run --bin graph-gateway ../../gateway.toml
