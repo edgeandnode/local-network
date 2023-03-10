634000393ae89b49e4dfde2aee0ea2560866f06b
Theo Butler <theodusbutler@gmail.com>
2023-03-09 21:11:45 -0500

    Update gateway config

--- gateway.sh
+++ gateway.sh
@@ -11,9 +11,10 @@ await_ready graph-subgraph
 await "test -f build/studio-admin-auth.txt"
 
 export STUDIO_AUTH=$(cat build/studio-admin-auth.txt)
-envsubst <gateway.toml >build/gateway.toml
+envsubst <gateway.jsonnet >build/gateway.jsonnet
+jsonnet build/gateway.jsonnet >build/gateway.json
 
 cd build/edgeandnode/graph-gateway
 
 export RUST_LOG=info,graph_gateway=trace
-cargo run --bin graph-gateway ../../gateway.toml
+cargo run --bin graph-gateway ../../gateway.json
