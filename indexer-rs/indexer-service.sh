a856cffacee22fb4120b79e0f84556cd7ed66bc8
Jannis Pohlmann <jannis@edgeandnode.com>
2023-12-05 16:27:23 +0100

    fix: improve indexer-rs service script

--- indexer-rs/indexer-service.sh
+++ indexer-rs/indexer-service.sh
@@ -8,26 +8,38 @@ fi
 
 . ./.env
 
-echo "awaiting controller"
-until curl -s "http://${DOCKER_GATEWAY_HOST}:${CONTROLLER}" >/dev/null; do sleep 1; done
-
-echo "awaiting postgres"
-until curl -s "http://${DOCKER_GATEWAY_HOST}:${POSTGRES}"; [ $? = 52 ]; do sleep 1; done
-
 host="${DOCKER_GATEWAY_HOST:-host.docker.internal}"
 echo "host=${host}"
 
+echo "awaiting controller"
+until curl -s "http://${host}:${CONTROLLER}" >/dev/null; do sleep 1; done
+
+echo "awaiting postgres"
+until curl -s "http://${host}:${POSTGRES}"; [ $? = 52 ]; do sleep 1; done
+
+echo "awaiting network subgraph"
 network_subgraph="$(curl "http://${host}:${CONTROLLER}/graph_subgraph")"
 echo "network_subgraph=${network_subgraph}"
 
+echo "awaiting escrow subgraph"
 escrow_subgraph="$(curl "http://${host}:${CONTROLLER}/escrow_subgraph")"
 echo "escrow_subgraph=${escrow_subgraph}"
+
+echo "awaiting scalar-tap-contracts"
+curl "http://${host}:${CONTROLLER}/scalar_tap_contracts" >scalar_tap_contracts.json
+
+tap_verifier=$(cat scalar_tap_contracts.json | jq -r '.tap_verifier')
+echo "tap_verifier=${tap_verifier}"
 
 cd build/graphprotocol/indexer-rs
 cat <<-EOT > config.toml
 [common.indexer]
 indexer_address = "${ACCOUNT0_ADDRESS}"
 operator_mnemonic = "${MNEMONIC}"
+
+[common.graph_node]
+status_url = "http://${host}:${GRAPH_NODE_STATUS}/graphql"
+query_base_url = "http://${host}:${GRAPH_NODE_GRAPHQL}"
 
 [common.database]
 postgres_url = "postgresql://dev@${host}:${POSTGRES}/indexer_components_0"
@@ -44,9 +56,14 @@ syncing_interval = 60
 id = 1
 
 [common.server]
-url_prefix = "/subgraphs"
+url_prefix = "/"
 host_and_port = "0.0.0.0:${INDEXER_SERVICE}"
 metrics_host_and_port = "0.0.0.0:${INDEXER_SERVICE_METRICS}"
+free_query_auth_token = "foo"
+
+[common.scalar]
+chain_id = 1
+receipts_verifier_address = "${tap_verifier}"
 EOT
 
 cat config.toml
