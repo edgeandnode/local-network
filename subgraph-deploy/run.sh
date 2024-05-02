3e0afb513879aab3c8f7f4871f5d8110bd6d97e3
Theo Butler <theodusbutler@gmail.com>
2024-05-02 19:30:42 -0400

    add subgraph-deploy

---
+++ subgraph-deploy/run.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -eu
+. /opt/.env
+
+# don't rerun when retriggered via a service_completed_successfully condition
+if curl "http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/name/graph-network" \
+  -H 'content-type: application/json' \
+  -d '{"query": "{ subgraphs { id } }" }' \
+  | grep "${SUBGRAPH}"
+then
+  exit 0
+fi
+
+network_subgraph_deployment="$(curl "http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/name/graph-network" \
+  -H 'content-type: application/json' \
+  -d '{"query": "{ _meta { deployment } }" }' \
+  | jq -r '.data._meta.deployment')"
+block_oracle_deployment="$(curl "http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/name/block-oracle" \
+  -H 'content-type: application/json' \
+  -d '{"query": "{ _meta { deployment } }" }' \
+  | jq -r '.data._meta.deployment')"
+
+# force index block oracle subgraph & network subgraph
+graph-indexer indexer connect "http://indexer-agent:${INDEXER_MANAGEMENT}"
+graph-indexer indexer --network=hardhat rules prepare "${network_subgraph_deployment}"
+graph-indexer indexer --network=hardhat rules prepare "${block_oracle_deployment}"
+
+deployment_hex="$(curl -X POST "http://ipfs:${IPFS_RPC}/api/v0/cid/format?arg=${block_oracle_deployment}&b=base16" \
+  | jq -r '.Formatted')"
+deployment_hex="${deployment_hex#f01701220}"
+echo "deployment_hex=${deployment_hex}"
+gns="$(jq -r '."1337".L1GNS.address' /opt/graph-contracts.json)"
+cast send --rpc-url="http://chain:${CHAIN_RPC}" --confirmations=0 --mnemonic="${MNEMONIC}" \
+  "${gns}" 'publishNewSubgraph(bytes32,bytes32,bytes32)' \
+  "0x${deployment_hex}" \
+  '0x0000000000000000000000000000000000000000000000000000000000000000' \
+  '0x0000000000000000000000000000000000000000000000000000000000000000'
+
+graph-indexer indexer --network=hardhat rules set "${block_oracle_deployment}" decisionBasis always
+
+# mine blocks in case the indexer-agent is stuck waiting for registration confirmations
+while ! graph-indexer indexer --network=hardhat actions get | grep 'allocate'; do
+  cast rpc --rpc-url="http://chain:${CHAIN_RPC}" evm_mine
+  sleep 2
+done
