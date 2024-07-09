fdeef8cd282cca6fc258d102259f8b74654aeb99
Daniel Werner <dan.werner@gmail.com>
2024-07-09 09:02:04 -0700

    bring down override containers first

--- up-overrides.sh
+++ up-overrides.sh
@@ -2,7 +2,6 @@
 
 export INDEXER_SERVICE_SOURCE_ROOT=$1
 export INDEXER_AGENT_SOURCE_ROOT=$1
-docker compose down
 docker compose -f docker-compose.yaml \
 -f overrides/indexer-service-ts-dev/indexer-service-ts-dev.yaml \
 -f overrides/indexer-agent-dev/indexer-agent-dev.yaml \
