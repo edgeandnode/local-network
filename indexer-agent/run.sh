9e3a0cebc284ecf1aeb38ee17b0f15fec6a6588d
Theo Butler <theodusbutler@gmail.com>
2025-02-12 08:47:52 -0500

    indexer-agent: fix indexer URL

--- indexer-agent/run.sh
+++ indexer-agent/run.sh
@@ -42,7 +42,7 @@ export INDEXER_AGENT_POSTGRES_HOST=postgres
 export INDEXER_AGENT_POSTGRES_PORT="${POSTGRES}"
 export INDEXER_AGENT_POSTGRES_USERNAME=postgres
 export INDEXER_AGENT_POSTGRES_PASSWORD=
-export INDEXER_AGENT_PUBLIC_INDEXER_URL="http://indexer-service-rs:${INDEXER_SERVICE}"
+export INDEXER_AGENT_PUBLIC_INDEXER_URL="http://indexer-service:${INDEXER_SERVICE}"
 export INDEXER_AGENT_TAP_SUBGRAPH_ENDPOINT="http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/semiotic/tap"
 mkdir -p ./config/
 cat >./config/config.yaml <<-EOF
