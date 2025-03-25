230d1bef00171049b6af8ad73278a7f39bb38c61
Lorenzo Delgado <lorenzo@edgeandnode.com>
2025-03-25 13:35:49 +0100

    lnsd/dips-debug
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- scripts/add-subgraph.sh
+++ scripts/add-subgraph.sh
@@ -1,24 +1,29 @@
+#!/bin/env bash
 set -xe
-source .env
+source "$PWD/.env"
+
 name=$1
 if [ -z "$name" ]; then
+  echo "Add a subgraph to the graph-network"
   echo "Usage: $0 <name>"
+  echo "  <name> The name of the subgraph in the graph-node"
   exit 1
 fi
+
+# Get the deployment hash from the graph-node
 deployment="$(curl -s "http://localhost:${GRAPH_NODE_GRAPHQL}/subgraphs/name/$name" \
   -H 'content-type: application/json' \
   -d '{"query": "{ _meta { deployment } }" }' \
   | jq -r '.data._meta.deployment')"
 echo "deployment=${deployment}"
+
+# Extract the deployment hash from the IPFS CID and strip the IPFS CID prefix
 deployment_hex="$(curl -s -X POST "http://localhost:${IPFS_RPC}/api/v0/cid/format?arg=${deployment}&b=base16" \
-  | jq -r '.Formatted')"
-
-# Remove the first 8 bytes of the hex string matching the IPFS prefix
-deployment_hex="${deployment_hex#f01701220}"
-
+  | jq -r '.Formatted' | sed 's/^f01701220//')"
 echo "deployment_hex=${deployment_hex}"
-gns="$(jq -r '."1337".L1GNS.address' contracts.json)"
 
+# Get the GNS address for the chain
+gns="$(jq -r ".\"${CHAIN_ID}\".L1GNS.address" contracts.json)"
 # https://github.com/graphprotocol/contracts/blob/3eb16c80d4652c238d3e6b2c396da712af5072b4/packages/sdk/src/deployments/network/actions/gns.ts#L38
 cast send --rpc-url="http://localhost:${CHAIN_RPC}" --confirmations=0 --mnemonic="${MNEMONIC}" \
   "${gns}" 'publishNewSubgraph(bytes32,bytes32,bytes32)' \
@@ -33,6 +38,6 @@ echo "Now run graph indexer to allocate to this subgraph ${deployment} :"
 echo "------------------------------------------------------------------"
 echo
 echo "./bin/graph-indexer indexer connect \"http://localhost:${INDEXER_MANAGEMENT}\""
-echo "./bin/graph-indexer indexer actions queue allocate ${deployment} 0.001 --network=hardhat"
+echo "./bin/graph-indexer indexer actions queue allocate ${deployment} 0.001 --network=${CHAIN_NAME}"
 echo "./bin/graph-indexer indexer actions get"
 echo "./bin/graph-indexer indexer actions update --id [id] status approved"
