9406fdbb15561a354e055a4e1c64394bd4afcb02
Tomás Migone <tomas@edgeandnode.com>
2025-04-01 17:37:05 -0300

    feat: add network subgraph support
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- graph-contracts/run.sh
+++ graph-contracts/run.sh
@@ -12,27 +12,23 @@ then
 fi
 
 # == DEPLOY PROTOCOL WITH SUBGRAPH SERVICE ==
-export SECURE_ACCOUNTS_DISABLE_PROVIDER=true
-export HARDHAT_VAR_LOCALHOST_RPC=http://chain:8545
-export HARDHAT_VAR_LOCALHOST_CHAIN_ID=1337
-
 if [ -n "${FORK_RPC_URL:-}" ] && [ -n "${HARDHAT_VAR_LOCALHOST_ACCOUNTS_MNEMONIC:-}" ]; then
   echo "FORK_RPC_URL detected, upgrading current version of the protocol"
 
   cd /opt/contracts/packages
-  cd horizon && npx hardhat deploy:migrate --network localhost --step 1 && cd ..
-  cd subgraph-service && npx hardhat deploy:migrate --network localhost --step 1 && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localhost --step 2 --patch-config && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localhost --step 3 --patch-config && cd ..
-  cd subgraph-service && npx hardhat deploy:migrate --network localhost --step 2 --patch-config && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localhost --step 4 --patch-config && cd ..
+  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 1 && cd ..
+  cd subgraph-service && npx hardhat deploy:migrate --network localNetwork --step 1 && cd ..
+  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 2 --patch-config && cd ..
+  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 3 --patch-config && cd ..
+  cd subgraph-service && npx hardhat deploy:migrate --network localNetwork --step 2 --patch-config && cd ..
+  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 4 --patch-config && cd ..
 else
   echo "No FORK_RPC_URL detected, deploying new version of the protocol"
   cd /opt/contracts/packages/subgraph-service
-  npx hardhat deploy:protocol --network localhost --subgraph-service-config local-network
+  npx hardhat deploy:protocol --network localNetwork --subgraph-service-config localNetwork
 fi
 
-jq -s '.[0] * .[1]' /opt/contracts/packages/subgraph-service/addresses-localhost.json /opt/contracts/packages/horizon/addresses-localhost.json > /opt/contracts.json
+jq -s '.[0] * .[1]' /opt/contracts/packages/subgraph-service/addresses-local-network.json /opt/contracts/packages/horizon/addresses-local-network.json > /opt/contracts.json
 
 # TODO: add back this assertion section once the deployment is stable
 # cat addresses-local.json
@@ -52,14 +48,20 @@ jq -s '.[0] * .[1]' /opt/contracts/packages/subgraph-service/addresses-localhost
 # printf "\naddresses match"
 
 # == DEPLOY NETWORK SUBGRAPH ==
-# cd /opt/graph-network-subgraph
-# sed -i 's/sepolia/hardhat/g' ./config/sepoliaAddressScript.ts
-# sed -i 's/11155111/1337/g' ./config/sepoliaAddressScript.ts
-# sed -i 's+@graphprotocol/contracts/addresses.json+/opt/contracts.json+g' ./config/sepoliaAddressScript.ts
-# sed -i 's/4454000/1/g' ./config/sepoliaAddressScript.ts
-# npx ts-node config/sepoliaAddressScript.ts
-# npx mustache ./config/generatedAddresses.json ./config/addresses.template.ts > ./config/addresses.ts
-# npx mustache ./config/generatedAddresses.json subgraph.template.yaml > subgraph.yaml
-# npx graph codegen --output-dir src/types/
-# npx graph create graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}"
-# npx graph deploy graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}" --ipfs="http://ipfs:${IPFS_RPC}" --version-label=v0.0.1
+cd /opt/graph-network-subgraph
+sed -i 's/arbitrum-sepolia/hardhat/g' ./config/arbitrumSepoliaAddressScript.ts
+sed -i 's/arbsep/localnetwork/g' ./config/arbitrumSepoliaAddressScript.ts
+sed -i 's/421614/1337/g' ./config/arbitrumSepoliaAddressScript.ts
+sed -i 's/570450/1/g' ./config/arbitrumSepoliaAddressScript.ts
+sed -i 's+@graphprotocol/contracts/addresses.json+/opt/contracts.json+g' ./config/arbitrumSepoliaAddressScript.ts
+# TODO: remove this once the network subgraph scripts are updated
+# For now we don't care about the L2GNS, SubgraphNFT, ServiceRegistry so we just use HorizonStaking address for them
+jq '.["1337"] = (.["1337"] * {"L2Staking": .["1337"].HorizonStaking, "L2GNS": .["1337"].HorizonStaking, "SubgraphNFT": .["1337"].HorizonStaking, "ServiceRegistry": .["1337"].HorizonStaking})' /opt/contracts.json > /opt/contracts.json.tmp
+cat /opt/contracts.json.tmp > /opt/contracts.json && rm /opt/contracts.json.tmp
+
+npx ts-node config/arbitrumSepoliaAddressScript.ts
+npx mustache ./config/generatedAddresses.json ./config/addresses.template.ts > ./config/addresses.ts
+npx mustache ./config/generatedAddresses.json subgraph.template.yaml > subgraph.yaml
+npx graph codegen --output-dir src/types/
+npx graph create graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}"
+npx graph deploy graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}" --ipfs="http://ipfs:${IPFS_RPC}" --version-label=v0.0.1
