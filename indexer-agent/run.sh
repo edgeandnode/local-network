a8c25cf1c8817582e4d6f0823cde66a1b02d2211
Pablo Carranza Velez <pablo@edgeandnode.com>
2025-05-27 14:27:24 +0000

    fix: newer agent, agent config, localhost only

--- indexer-agent/run.sh
+++ indexer-agent/run.sh
@@ -44,6 +44,11 @@ export INDEXER_AGENT_POSTGRES_USERNAME=postgres
 export INDEXER_AGENT_POSTGRES_PASSWORD=
 export INDEXER_AGENT_PUBLIC_INDEXER_URL="http://indexer-service:${INDEXER_SERVICE}"
 export INDEXER_AGENT_TAP_SUBGRAPH_ENDPOINT="http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/semiotic/tap"
+export INDEXER_AGENT_ENABLE_DIPS=true
+export INDEXER_AGENT_DIPS_EPOCHS_MARGIN=1
+export INDEXER_AGENT_DIPPER_ENDPOINT="http://dipper:${DIPPER_INDEXER_RPC_PORT}"
+export INDEXER_AGENT_DIPS_ALLOCATION_AMOUNT=1
+
 mkdir -p ./config/
 cat >./config/config.yaml <<-EOF
 networkIdentifier: "hardhat"
