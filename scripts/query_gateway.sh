ebdb9ef7a19a4cfd7c6ce21cd838b7e736f89e44
Tomás Migone <tomas@edgeandnode.com>
2025-09-09 13:39:15 -0300

    chore: update query script
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- scripts/query_gateway.sh
+++ scripts/query_gateway.sh
@@ -8,10 +8,11 @@
 
 # Number of times to run the commands, default is 1
 count=${1:-1}
+deployment_id=${2:-"BFr2mx7FgkJ36Y6pE5BiXs1KmNUmVDCnL82KUSdcLW1g"}
 
 for ((i=0; i<count; i++))
 do
-    curl "http://localhost:7700/api/subgraphs/id/BFr2mx7FgkJ36Y6pE5BiXs1KmNUmVDCnL82KUSdcLW1g" \
+    curl "http://localhost:7700/api/subgraphs/id/${deployment_id}" \
         -H 'content-type: application/json' \
         -H "Authorization: Bearer deadbeefdeadbeefdeadbeefdeadbeef" \
         -d '{"query": "{ _meta { block { number } } }"}'
