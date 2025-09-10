c9af556986a56ff484c5607678980157728c3eb4
Tomás Migone <tomas@edgeandnode.com>
2025-09-10 10:49:36 -0300

    fix: use signer key for tap-aggregator instead of sender key ...
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- tap-aggregator/run.sh
+++ tap-aggregator/run.sh
@@ -6,7 +6,7 @@ tap_verifier="$(jq -r '."1337".TAPVerifier' /opt/tap-contracts.json)"
 graph_tally_verifier="$(jq -r '."1337".GraphTallyCollector.address' /opt/horizon.json)"
 
 export TAP_PORT="${TAP_AGGREGATOR}"
-export TAP_PRIVATE_KEY="${ACCOUNT0_SECRET}"
+export TAP_PRIVATE_KEY="${ACCOUNT1_SECRET}"
 export TAP_DOMAIN_CHAIN_ID=1337
 export TAP_DOMAIN_VERIFYING_CONTRACT="${tap_verifier}"
 export TAP_DOMAIN_VERIFYING_CONTRACT_V2="${graph_tally_verifier}"
