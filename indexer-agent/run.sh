22e88331ec922e45ec78278cc9dd70d7a4d798b1
Tomás Migone <tomas@edgeandnode.com>
2025-09-16 18:05:49 -0300

    chore: bump contracts version
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- indexer-agent/run.sh
+++ indexer-agent/run.sh
@@ -21,7 +21,6 @@ if [ "${indexer_staked}" = "false" ]; then
     "${staking_address}" 'stake(uint256)' '100000000000000000000000'
 fi
 
-cd /opt/indexer/packages/indexer-agent
 export INDEXER_AGENT_HORIZON_ADDRESS_BOOK=/opt/horizon.json
 export INDEXER_AGENT_SUBGRAPH_SERVICE_ADDRESS_BOOK=/opt/subgraph-service.json
 export INDEXER_AGENT_TAP_ADDRESS_BOOK=/opt/tap-contracts.json
@@ -35,6 +34,7 @@ export INDEXER_AGENT_INDEXER_ADDRESS="${RECEIVER_ADDRESS}"
 export INDEXER_AGENT_INDEXER_MANAGEMENT_PORT="${INDEXER_MANAGEMENT}"
 export INDEXER_AGENT_INDEX_NODE_IDS=default
 export INDEXER_AGENT_INDEXER_GEO_COORDINATES="1 1"
+export INDEXER_AGENT_VOUCHER_REDEMPTION_THRESHOLD=0.01
 export INDEXER_AGENT_NETWORK_SUBGRAPH_ENDPOINT="http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/name/graph-network"
 export INDEXER_AGENT_NETWORK_PROVIDER="http://chain:${CHAIN_RPC}"
 export INDEXER_AGENT_MNEMONIC="${INDEXER_MNEMONIC}"
@@ -47,24 +47,6 @@ export INDEXER_AGENT_PUBLIC_INDEXER_URL="http://indexer-service:${INDEXER_SERVIC
 export INDEXER_AGENT_TAP_SUBGRAPH_ENDPOINT="http://graph-node:${GRAPH_NODE_GRAPHQL}/subgraphs/semiotic/tap"
 export INDEXER_AGENT_MAX_PROVISION_INITIAL_SIZE=200000
 export INDEXER_AGENT_CONFIRMATION_BLOCKS=1
-
-mkdir -p ./config/
-cat >./config/config.yaml <<-EOF
-networkIdentifier: "hardhat"
-indexerOptions:
-  geoCoordinates: [48.4682, -123.524]
-  defaultAllocationAmount: 10000
-  allocationManagementMode: "auto"
-  restakeRewards: true
-  poiDisputeMonitoring: false
-  voucherRedemptionThreshold: 0.00001
-  voucherRedemptionBatchThreshold: 10
-  rebateClaimThreshold: 0.00001
-  rebateClaimBatchThreshold: 10
-subgraphs:
-  maxBlockDistance: 5000
-  freshnessSleepMilliseconds: 1000
-EOF
-cat config/config.yaml
+export INDEXER_AGENT_LOG_LEVEL=trace
 
 node ./dist/index.js start
