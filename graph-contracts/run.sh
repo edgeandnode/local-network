600b79179eb7607f001768e55e893f3b855edffe
Tomás Migone <tomas@edgeandnode.com>
2025-09-18 13:06:00 -0300

    fix: contracts not starting
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- graph-contracts/run.sh
+++ graph-contracts/run.sh
@@ -33,41 +33,30 @@ cd horizon && echo "{}" > addresses-local-network.json && cd ..
 cd subgraph-service && echo "{}" > addresses-local-network.json && cd ..
 
 # == DEPLOY PROTOCOL WITH SUBGRAPH SERVICE ==
-if [ -n "${FORK_RPC_URL:-}" ]; then
-  # TODO: remove this after horizon. This is only useful for testing the ugprade.
-  echo "FORK_RPC_URL detected, upgrading current version of the protocol"
-  cd /opt/contracts/packages
-
-  # Transfer ownership of protocol to hardhat signer 1
-  cd horizon && npx hardhat test:transfer-ownership --network localNetwork && cd ..
-
-  # Now we can upgrade the protocol
-  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 1 && cd ..
-  cd subgraph-service && npx hardhat deploy:migrate --network localNetwork --step 1 && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 2 --patch-config --account-index 1 && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 3 --patch-config && cd ..
-  cd subgraph-service && npx hardhat deploy:migrate --network localNetwork --step 2 --patch-config && cd ..
-  cd horizon && npx hardhat deploy:migrate --network localNetwork --step 4 --patch-config --account-index 1 && cd ..
-else
-  echo "No FORK_RPC_URL detected, deploying new version of the protocol"
-  cd /opt/contracts/packages/subgraph-service
-  npx hardhat deploy:protocol --network localNetwork --subgraph-service-config localNetwork
-fi
+echo "No FORK_RPC_URL detected, deploying new version of the protocol"
+cd /opt/contracts/packages/subgraph-service
+npx hardhat deploy:protocol --network localNetwork --subgraph-service-config localNetwork
 
 # == DEPLOY NETWORK SUBGRAPH ==
 cp /opt/contracts/packages/horizon/addresses-local-network.json /opt/horizon.json
 cp /opt/contracts/packages/subgraph-service/addresses-local-network.json /opt/subgraph-service.json
 cd /opt/graph-network-subgraph
 
+# Patch subgraph service address book, add "legacy" contracts to avoid network subgraph from crashing
+jq '.["1337"] += {
+  "LegacyServiceRegistry": { "address": "0x0000000000000000000000000000000000000000" },
+  "LegacyDisputeManager": { "address": "0x0000000000000000000000000000000000000000" }
+}' /opt/subgraph-service.json > /opt/tmp.json && mv /opt/tmp.json /opt/subgraph-service.json
+
 # Build and deploy the subgraph
 npx ts-node config/localNetworkAddressScript.ts
 npx mustache ./config/generatedAddresses.json ./config/addresses.template.ts > ./config/addresses.ts
 npx mustache ./config/generatedAddresses.json subgraph.template.yaml > subgraph.yaml
+echo -e "\n== Subgraph manifest ==\n"
+cat subgraph.yaml
 npx graph codegen --output-dir src/types/
 npx graph create graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}"
 npx graph deploy graph-network --node="http://graph-node:${GRAPH_NODE_ADMIN}" --ipfs="http://ipfs:${IPFS_RPC}" --version-label=v0.0.1
-
-cat subgraph.yaml
 
 # Keep the container running - for development purposes
 if [ -n "${KEEP_CONTAINER_RUNNING:-}" ]; then
