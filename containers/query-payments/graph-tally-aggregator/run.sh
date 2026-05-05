1b292eb6fbcbc02f11a3171113b9c66cc4f993cb
Tomás Migone <tomas@edgeandnode.com>
2026-05-05 13:43:52 -0300

    fix: rename fixes
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- containers/query-payments/graph-tally-aggregator/run.sh
+++ containers/query-payments/graph-tally-aggregator/run.sh
@@ -9,6 +9,6 @@ graph_tally_verifier=$(contract_addr GraphTallyCollector.address horizon)
 export GRAPH_TALLY_PORT="${GRAPH_TALLY_AGGREGATOR_PORT}"
 export GRAPH_TALLY_PRIVATE_KEY="${ACCOUNT1_SECRET}"
 export GRAPH_TALLY_DOMAIN_CHAIN_ID=1337
-export GRAPH_TALLY_DOMAIN_VERIFYING_CONTRACT_V2="${graph_tally_verifier}"
+export GRAPH_TALLY_DOMAIN_VERIFYING_CONTRACT="${graph_tally_verifier}"
 
 graph_tally_aggregator
